c46818354da2c74d9096cb35f2657b3a1e9b0946
Brendon McBain <brendon.mcbain9@gmail.com>
2017-12-21 00:13:17 +1100

    Documentation
    Added documentation and VS 2017 solution.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # RaceTelemetry
-LED rev lights for the racing game F1 2017. Developed in Visual Studio 2017 RC Edition and runs with the Arduino Uno on pins two to eleven.
+Interface Adafruit NeoPixel Strip and 7 Segment Display hardware with F1 2017, displaying the rev limiter and current gear of the car using an Arduino Pro Micro.
+
+# Project files
+RaceTelemetry.cpp is the main project file which creates a UDP server to receive data from the UDP client in the F1 2017 racing game.
+After receiving new data, this file will then send serial data through USB using the SerialPort.h library to the Arduino with RaceTelemetry.ino uploaded.
+
+# Getting started
+1.  Connect your Arduino and open the Arduino IDE
+2.  Compile and upload RaceTelemetry.ino, noting the COM port used.
+3.  Change PORT_NAME to the COM port your Arduino is connected to.
+4.  Build and compile the RaceTelemetry solution.
+5.  Run the program, and open an F1 2017 session.
+
+# Environment
+The RaceTelemetry solution was created in Visual Studio 2017 RC (v141 toolset) on Windows 10, hence Winsock.h was the primary library for creating the server.
+The Arduino sketch was created in Arduino IDE 1.0.6.
---
+++ RaceTelemetry/RaceTelemetry/SerialPort.h
@@ -0,0 +1,31 @@
+// ****************************************************************************
+// **** Author: Manash Kumar Mandal (commented by Brendon McBain)
+// **** Source: https://blog.manash.me/serial-communication-with-an-arduino-using-c-on-windows-d08710186498
+// ****************************************************************************
+
+#ifndef SERIALPORT_H		// Header guard
+#define SERIALPORT_H
+
+#define ARDUINO_WAIT_TIME 2000
+#define MAX_DATA_LENGTH 2	// Number of bytes to send
+
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+class SerialPort {
+	private:
+		HANDLE handler;					// Serial comm handler
+		bool connected;					// Connection status
+		COMSTAT status;					// Get various information about the connection
+		DWORD errors;					// Keep track of last error
+	public:
+		SerialPort(char *portName);		// Initialise Serial communication with the given COM port
+		~SerialPort();					// Close the connection
+
+		int readSerialPort(char *buffer, unsigned int buf_size);	// Read data in a buffer
+		bool writeSerialPort(char *buffer, unsigned int buf_size);  // Writes data from a buffer through the Serial connection
+		bool isConnected();											// Check if we are actually connected
+};
+
+#endif // SERIALPORT_H
